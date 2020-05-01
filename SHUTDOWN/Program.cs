6e2268d31be72f066d1bb8beb64e59df0e7c1e3c
szatan667 <kubakocoj@gmail.com>
2020-05-01 20:06:52 +0200

    Added shutdown mode (shutdown or restart)

--- SHUTDOWN/Program.cs
+++ SHUTDOWN/Program.cs
@@ -18,6 +18,7 @@ public class SHUTDOWNX : ApplicationContext
     enum ShutdownOptions
     {
         Shutdown,
+        Restart,
         Abort
     }
     /// <summary>
@@ -52,31 +53,41 @@ public class SHUTDOWNX : ApplicationContext
             Visible = true,
             ContextMenu = new ContextMenu(new MenuItem[]
             //Add menu items and set their properties and events
+            //Use "Tag" property to store custom parameters (shutdown time or shutdown option)
             {
                 new MenuItem("Pick schedule:")
                 {
                     Enabled = false,
                 },
-                new MenuItem("Now", EventCheck)
+                new MenuItem("Now", EventCheckMark)
                 {
                     Tag = ShutdownTime.Now,
                     RadioCheck = true,
                 },
-                new MenuItem("1 hour", EventCheck)
+                new MenuItem("1 hour", EventCheckMark)
                 {
                     Tag = ShutdownTime.Hrs1,
                     RadioCheck = true,
                     Checked = true
                 },
-                new MenuItem("2 hours", EventCheck)
+                new MenuItem("2 hours", EventCheckMark)
                 {
                     Tag = ShutdownTime.Hrs2,
                     RadioCheck = true,
                 },
-                new MenuItem("6 hours", EventCheck)
+                new MenuItem("6 hours", EventCheckMark)
                 {
                     Tag = ShutdownTime.Hrs6,
                     RadioCheck = true
+                },
+                new MenuItem("-"),
+                new MenuItem("Shutdown action", new MenuItem[] //this is menu item with submenu
+                {
+                    new MenuItem("Shutdown", EventCheckMark) {RadioCheck = true, Checked = true, Tag = ShutdownOptions.Shutdown },
+                    new MenuItem("Restart", EventCheckMark) {RadioCheck = true, Tag = ShutdownOptions.Restart }
+                })
+                {
+                    Name = "ShutdownAction"
                 },
                 new MenuItem("-"),
                 new MenuItem("SHUTDOWN!", EventShutdown),
@@ -89,30 +100,31 @@ public class SHUTDOWNX : ApplicationContext
         //Register tray icon click event
         trayIcon.MouseClick += EventIconClick;
     }
-    //Shutdown time menu interval handler
-    private void EventCheck(object ClickedItem, EventArgs e)
+
+    //Set check mark for menu items within one group
+    private void EventCheckMark(object ClickedItem, EventArgs e)
     {
+        //Go through event sender parent's menu items
         //Don't look for currently checked item - just clear them all first...
-        foreach (MenuItem mi in trayIcon.ContextMenu.MenuItems)
-        {
+        foreach (MenuItem mi in (ClickedItem as MenuItem).Parent.MenuItems)
             mi.Checked = false;
-        }
 
         //...and then mark desired as checked
-        //(ClickedItem is menu item that triggered this event, we can set it's property here directly)
         (ClickedItem as MenuItem).Checked = true;
     }
+
     //Tray icon click handler
     private void EventIconClick(object s, MouseEventArgs e)
     {
         if (e.Button == MouseButtons.Left)
             trayIcon.ShowBalloonTip(0);
     }
-    //Shutdown menu handler
+
+    //Shutdown action menu handler
     void EventShutdown(object s, EventArgs e)
     {
-        //Find checked menu item and fire up shutdown with tagged time value
-        //(Tag property is set to ShutdownTime type in tray icon constructor)
+        //Find checked menu item and fire up shutdown with proper option and time
+        //(get option and time from enu item "Tag" properties)
         foreach (MenuItem mi in trayIcon.ContextMenu.MenuItems)
             if (mi.Checked)
             {
@@ -122,12 +134,19 @@ public class SHUTDOWNX : ApplicationContext
                 trayIcon.BalloonTipTitle = trayIcon.Text;
                 trayIcon.BalloonTipText = "Right-click tray icon to abort scheduled shutdown.";
 
-                Shutdown(ShutdownOptions.Shutdown, (ShutdownTime)mi.Tag);
+                //Now get picked item from shutdown options
+                foreach (MenuItem mi2 in trayIcon.ContextMenu.MenuItems["ShutdownAction"].MenuItems)
+                    if (mi2.Checked)
+                    {
+                        Shutdown((ShutdownOptions)mi2.Tag, (ShutdownTime)mi.Tag);
+                        break;
+                    }
 
                 break;
             }
     }
-    //Abort menu handler
+
+    //Abort action menu handler
     void EventAbort(object s, EventArgs e)
     {
         trayIcon.Icon = SHUTDOWN.Properties.Resources.GREEN;
@@ -137,6 +156,7 @@ public class SHUTDOWNX : ApplicationContext
 
         Shutdown(ShutdownOptions.Abort, ShutdownTime.Now);
     }
+
     //Exit menu handler
     void EventExit(object s, EventArgs e)
     {
@@ -163,6 +183,9 @@ public class SHUTDOWNX : ApplicationContext
             {
                 case ShutdownOptions.Shutdown:
                     p.StartInfo.Arguments = string.Join(" ", "/s", "/t", ((int)ShutdownTime).ToString());
+                    break;
+                case ShutdownOptions.Restart:
+                    p.StartInfo.Arguments = string.Join(" ", "/r", "/t", ((int)ShutdownTime).ToString());
                     break;
                 case ShutdownOptions.Abort:
                     p.StartInfo.Arguments = "/a";
